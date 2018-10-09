07442152f6a9425bef1678c5b3dd227dce3f43ab
Your Name <you@example.com>
2018-10-09 21:39:42 +0330

    Added Contact Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    // return React.createElement('div',{
+    //   className:'App'
+    // },React.createElement('h1',{},'This is a sample'));
+    const Name = "Hamed Abasi";
+    const showName = true;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>This is a sample,
+            {showName ? 'Mr '+Name.toLowerCase() : null}
+             . {34 + 1000}</h1>
       </div>
     );
   }
