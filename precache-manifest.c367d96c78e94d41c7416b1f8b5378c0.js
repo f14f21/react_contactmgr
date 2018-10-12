1c1701c30040ca7fc5f8c3bd5c1cba1aaeb137f0
Your Name <you@example.com>
2018-10-12 14:25:06 +0330

    Updates

---
+++ precache-manifest.c367d96c78e94d41c7416b1f8b5378c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "42dc60102e359ea4e090",
+    "url": "/static/js/main.42dc6010.chunk.js"
+  },
+  {
+    "revision": "6f94de0bbee8b24b23f4",
+    "url": "/static/js/1.6f94de0b.chunk.js"
+  },
+  {
+    "revision": "42dc60102e359ea4e090",
+    "url": "/static/css/main.f2b6c741.chunk.css"
+  },
+  {
+    "revision": "6f94de0bbee8b24b23f4",
+    "url": "/static/css/1.1b776b09.chunk.css"
+  },
+  {
+    "revision": "bfc7f143dfb9c0dc9c11c2221d1a35db",
+    "url": "/index.html"
+  }
+];
